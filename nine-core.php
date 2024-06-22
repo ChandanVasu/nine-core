c0ff9a4b5ea077581b2a24181108e3f65d016ff8
Chandan Vasu <153380073+ChandanVasu@users.noreply.github.com>
2024-06-22 19:57:12 +0530

    Update nine-core.php

--- nine-core.php
+++ nine-core.php
@@ -1,62 +1,73 @@
 <?php
 /*
-Plugin Name: Nine Core
-Description: A simple plugin for demonstration.
-Version: 1.0.0
-Author: Vasu Theme
-Author URI: https://themeforest.net/user/vasutheme
-Requires Plugins: elementor
-*/
+ * Plugin Name:       Nine Core
+ * Plugin URI:        https://themeforest.net/user/vasutheme/
+ * Description:       This is core plugin for Nine theme
+ * Version:           1.0
+ * Author:            VasuTHeme
+ * Author URI:        https://themeforest.net/user/vasutheme
+ * License:           GPL-2.0+
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       nine-core
+ * Domain Path:       /languages
+ */
 
+
+// Define constant for plugin directory path
 define('NINE_DIR', plugin_dir_path(__FILE__));
 
-
-
-$current_theme = wp_get_theme();
-if ($current_theme->get('Name') == 'Nine') {
-    $files_to_include = [
-        'inc/template/override/header.php',
-        'assets/variable.php',
-        'inc/post/custom-post.php',
-        'inc/template/override/footer.php',
-        'inc/inc.php',
-        'inc/template/opstion/header.php',
-        'meta-box/init.php',
-        'elementor/control.php',
-        'elementor/inc/grid-one-load.php'
-    ];
-
-    foreach ($files_to_include as $file) {
-        require_once NINE_DIR . $file;
-    }
+// Include ReduxFramework if not already included
+if (!class_exists('ReduxFramework')) {
+    require_once dirname(__FILE__) . '/redux-core/framework.php';
 }
 
+// Check if the active theme is 'Nine'
+$current_theme = wp_get_theme();
+if ($current_theme->get('Name') == 'Nine') {
+    $plugin_path = plugin_dir_path(__FILE__);
+    require_once $plugin_path . 'inc/template/override/header.php';
+    require_once $plugin_path . 'assets/variable.php';
+    require_once $plugin_path . 'inc/post/custom-post.php';
+    require_once $plugin_path . 'inc/template/override/footer.php';
+    require_once $plugin_path . 'inc/inc.php';
+    require_once $plugin_path . 'inc/template/opstion/header.php';
+    require_once $plugin_path . '/meta-box/init.php';
+    require_once $plugin_path . '/elementor/control.php';
+    require_once $plugin_path . '/elementor/inc/grid-one-load.php';
+}
+
+
+// Enqueue main.css file
 function adjust_styles_load_order() {
     wp_dequeue_style('elementor-frontend');
     wp_enqueue_style('elementor-frontend');
 }
 add_action('wp_enqueue_scripts', 'adjust_styles_load_order', 99);
 
-if (!class_exists('ReduxFramework')) {
-    require_once NINE_DIR . 'redux-core/framework.php';
-}
-
+// Activation hook
 function nine_core_activate() {
-    
+    // Activation code here
 }
 register_activation_hook(__FILE__, 'nine_core_activate');
 
+// Enqueue styles
 function nine_core_enqueue_styles() {
     wp_enqueue_style('nine-core-main-style', plugins_url('assets/main.css', __FILE__));
-    // Uncomment to enqueue video style if needed
     // wp_enqueue_style('video-style', plugins_url('assets/video.css', __FILE__));
+
 }
 add_action('wp_enqueue_scripts', 'nine_core_enqueue_styles');
 
+// Enqueue load more script
 function enqueue_load_more_script() {
-    wp_enqueue_script('load-more-script', plugins_url('assets/js/main.js', __FILE__), ['jquery'], null, true);
-    // Uncomment to enqueue video script if needed
+    wp_enqueue_script('load-more-script', plugins_url('assets/js/main.js', __FILE__), array('jquery'), null, true);
     // wp_enqueue_script('video-script', plugins_url('assets/js/video.js', __FILE__));
     wp_localize_script('load-more-script', 'ajaxurl', admin_url('admin-ajax.php'));
+
+    // Enqueue the script
+    wp_enqueue_script('load-more-script');
 }
 add_action('wp_enqueue_scripts', 'enqueue_load_more_script');
+
+
+
